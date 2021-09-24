2dce1cf0ede137f39be1cd55a44e8df02a8ae68d
binaryintelligencelab <binaryintelligencelab@gmail.com>
2021-09-24 19:03:19 +0100

    update user

--- controllers/user.js
+++ controllers/user.js
@@ -77,6 +77,7 @@ function renderPost(viewModel,myCategorie,allCategorie,myClasse,res,req){
 	}else{
 		console.log("I am tired");
 		Article.find({},function(err,article){
+			console.log("inside");
 			if(err){
 				console.log("err");
 				console.log(err);
