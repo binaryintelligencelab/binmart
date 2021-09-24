ece0dc97e03e9fc35d0efc2edfab99602ff5dc92
binaryintelligencelab <binaryintelligencelab@gmail.com>
2021-09-24 17:22:52 +0100

    update root

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app=config(app);
 
 mongoose.Promise=global.Promise;
 
-mongoose.connect("mongodb+srv://binmart:<password>@cluster0.libcr.mongodb.net/binmartdatabase?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://binaryintelligencelab:AzN2XWuGUwY6pGOg@cluster0.libcr.mongodb.net/binmartdatabase?retryWrites=true&w=majority");
 
 mongoose.connection.on("open",function(){
 	console.log("app is connected"); 
